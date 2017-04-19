f37fafcf343d717c11c2c03a3708da7a9c4f60a1
Eg0Jemima <Eg0Jemima@gmail.com>
2017-04-19 12:52:34 -0400

    - Created TextRotator.cs that rotates letters around character

---
+++ Assets/TextRotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextRotator : MonoBehaviour
+{
+    public Transform target;
+
+    void Update()
+    {
+        Vector3 relativePos = ((target.position + new Vector3(0, 2.0f, 0f)) - transform.position);
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+
+        Quaternion current = transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
+        transform.Translate(0, 0, 10 * Time.deltaTime);
+    }
+}
