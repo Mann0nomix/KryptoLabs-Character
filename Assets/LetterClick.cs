2903d9ea5b53db3a4ea7c4d6da91ae64dc51357e
Eg0Jemima <Eg0Jemima@gmail.com>
2017-04-19 16:47:45 -0400

    - Added tag for character - Added Animate script to handle magic and jump animations for girl - Added LetterClick to start video when letter is clicked

---
+++ Assets/LetterClick.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterClick : MonoBehaviour {
+    public GameObject movie;
+
+    // NOTE: You need a collider in order to use raycast on character
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.tag == "Letter") {
+                    Debug.Log("This is a letter");
+                    StartVideo();
+                } else { //for any other colliders in scene
+                    Debug.Log("This isn't a letter");
+                }
+            }
+        }
+    }
+
+    void StartVideo()
+    {
+        if (!movie.activeInHierarchy) {
+            movie.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/AnimateGirl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateGirl : MonoBehaviour {
+    public GameObject magicLeft;
+    public GameObject magicRight;
+    public GameObject letter;
+    public GameObject movie;
+
+	// NOTE: You need a collider in order to use raycast on character
+	void Update () {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit)) {
+                if (hit.transform.tag == "MagicGirl") {
+                    Debug.Log("This is my girl");
+                    StartJump();
+                    StartMagic();
+                } else { //for any other colliders in scene
+                    Debug.Log("This isn't my girl");
+                }
+            }
+        }
+    }
+
+    void StartMagic()
+    {
+        if (!magicLeft.activeInHierarchy) {
+            magicLeft.SetActive(true);
+            magicRight.SetActive(true);
+            letter.SetActive(true);
+        }
+    }
+
+    void StartJump()
+    {
+        transform.Translate(transform.position + new Vector3(0f, -4.4f, 4.0f));
+    }
+}
