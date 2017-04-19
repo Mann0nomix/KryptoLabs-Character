3f5c6360afef57666d6df27a24896734914d2b8c
Mann, Eustace (Contractor) <Eustace_Mann@comcast.com>
2017-04-19 14:29:29 -0400

    - Created Movietexture that plays movie - For some reason the movie plays with a red background… why???

---
+++ Assets/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movie : MonoBehaviour {
+
+	public MovieTexture movTexture;
+
+	void Start() {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.Play();
+	}
+}
